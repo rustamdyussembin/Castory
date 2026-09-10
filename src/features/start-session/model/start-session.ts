540c285615b9e63e9c6b0403b5c22046b160c149
Rustam Dyussembin <rustamdyussembin@gmail.com>
2026-09-10 19:30:48 +0500

    update start session logic

--- src/features/start-session/model/start-session.ts
+++ src/features/start-session/model/start-session.ts
@@ -1,12 +1,6 @@
 import { create } from 'zustand';
 import { IStartSessionState } from '../start-session.types';
 
-interface IStartSessionSheetState {
-  isOpen: boolean;
-  open: () => void;
-  close: () => void;
-}
-
 export const useStartSessionStore = create<IStartSessionState>()((set) => ({
   data: null,
   isSheetOpen: false,
