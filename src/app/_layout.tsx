ac901d6405c80948b1536cc09919107fb7e7c81e
Rustam Dyussembin <rustamdyussembin@gmail.com>
2026-09-13 18:58:04 +0500

    remove theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { focusManager, QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { AppState, type AppStateStatus, Platform, useColorScheme } from 'react-native';
@@ -7,7 +7,6 @@ import { PaperProvider } from 'react-native-paper';
 
 import { I18nProvider } from '@/app/providers/i18n';
 import AppTabs from '@/app/tabs/app-tabs/app-tabs';
-import { navigationDarkTheme, navigationLightTheme, paperDarkTheme, paperLightTheme } from '@/shared/theme';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -15,7 +14,6 @@ const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const isDark = colorScheme === 'dark';
 
   useEffect(() => {
     if (Platform.OS === 'web') return;
@@ -36,8 +34,8 @@ export default function RootLayout() {
   return (
     <I18nProvider>
       <QueryClientProvider client={queryClient}>
-        <PaperProvider theme={isDark ? paperDarkTheme : paperLightTheme}>
-          <ThemeProvider value={isDark ? navigationDarkTheme : navigationLightTheme}>
+        <PaperProvider>
+          <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
             <AppTabs />
           </ThemeProvider>
         </PaperProvider>
--- src/shared/theme/index.ts
+++ src/shared/theme/index.ts
@@ -1,5 +1,4 @@
 export * from './colors';
 export * from './fonts';
-export * from './paper-theme';
 export * from './spacing';
 export * from './use-theme-colors';
